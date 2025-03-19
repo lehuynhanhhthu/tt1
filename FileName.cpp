804f8ed6340e126de207b652f03fa608a1d75a10
lehuynhanhhthu <anhthu1112006@gmail.com>
2025-03-19 13:19:41 +0700

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int a[100], n;
+void nhap() {
+	for (int i = 1; i <= n; i++) {
+		a[i] = 0;
+	}
+}
+void sinh() {
+
+	 }
+int main() {
+	
+		cout << "HELLO WORLD";
+		//system("pause");
+
+
+
+
+		return 0;
+	} 
